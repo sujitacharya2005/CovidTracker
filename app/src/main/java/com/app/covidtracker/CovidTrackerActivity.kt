b1cc59b888ef9e0012bd6f363a5950df25b5ea21
sujit <sujitacharya2005@gmail.com>
2020-11-08 10:21:57 +0530

    Collapsing Toolbar

--- app/src/main/java/com/app/covidtracker/CovidTrackerActivity.kt
+++ app/src/main/java/com/app/covidtracker/CovidTrackerActivity.kt
@@ -9,6 +9,7 @@ class CovidTrackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_covid_tracker)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
     }
-
 }
